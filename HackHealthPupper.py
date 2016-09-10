11d195d9e3a903e82bb90cb8bcc1b543848782c9
Elizabeth Williams <elizabeth.williams.1@stonybrook.edu>
2016-09-10 18:23:36 -0400

    top pups

--- HackHealthPupper.py
+++ HackHealthPupper.py
@@ -2,6 +2,7 @@ import praw, re, requests, os, glob, sys
 from flask import Flask, request, render_template
 import random as random
 from DoggoIdentifier import DoggoAPICall
+import operator
 '''
 setting up a reddit user for scraping pictures
 '''
@@ -16,6 +17,7 @@ MIN_SCORE = 1 # min score for pup pictures
 subreddit = "rarepuppers"
 old_doggo = "string"
 image_history = {}
+top_tags_dict = {}
 
 def yielding(ls):
     for i in ls:
@@ -57,23 +59,56 @@ def scrape_pic(old_doggo):
     return None
 
 
-
-
 '''
 setting up flask server stuff
 '''
 
 app = Flask(__name__)
 
-sounds = ["Dog.mp3", "Puppy.mp3", "Puppy2.mp3", "Dog2.mp3"]
+sounds = ["Dog.mp3","Puppy.mp3","Puppy2.mp3","Dog2.mp3"]
 
-
+current_doggo = None
 
 @app.route('/')
 def hello_world():
     doggo_object = scrape_pic(old_doggo)
+    global current_doggo
+    current_doggo = doggo_object
 
-    return render_template('pupperPage.html', sound=sounds[random.randint(0,3)],image_url=doggo_object.url, doggo_message=doggo_object.bork_message())
+    return render_template('pupperPage.html',values="", description="", sound=sounds[random.randint(0,3)], image_url=doggo_object.url, doggo_message=doggo_object.bork_message())
+
+@app.route('/uppup')
+def up_pup():
+    classes = current_doggo.get_classes()
+    for c in classes:
+        if c in top_tags_dict:
+            top_tags_dict[c] += 1
+        else:
+            top_tags_dict[c] = 1
+    print("hello")
+    print(top_tags_dict)
+    doggo_object = scrape_pic(old_doggo)
+    global current_doggo
+    current_doggo = doggo_object
+    return render_template('pupperPage.html', values ="",description="", sound=sounds[random.randint(0,3)],image_url=doggo_object.url, doggo_message=doggo_object.bork_message())
+
+@app.route('/toptags')
+def show_top_tags():
+    sorted_top_tags = sorted(top_tags_dict.items(), key=operator.itemgetter(1), reverse=True)
+
+    description = ""
+
+    values = ""
+
+    loop_range = 5
+    if len(sorted_top_tags) < 5:
+        loop_range = len(sorted_top_tags)
+
+    for i in range(loop_range):
+        description = description + sorted_top_tags[i][0] + " " 
+        values = values + str(sorted_top_tags[i][1]) + " "
+
+    return render_template('pupperPage.html',values=values, description=description, sound=sounds[random.randint(0,3)],image_url=current_doggo.url, doggo_message=current_doggo.bork_message())
 
 if __name__ == '__main__':
     app.run(debug=True)
