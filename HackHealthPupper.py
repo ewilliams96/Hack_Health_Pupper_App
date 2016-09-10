1a21014de2f0ebdb051b7853f10af18b5b36dafe
= <benjamin.kandov@stonybrook.edu>
2016-09-10 15:49:51 -0400

    shuffle fixed

--- HackHealthPupper.py
+++ HackHealthPupper.py
@@ -14,21 +14,21 @@ user_agent = "pupper_finder"
 reddit = praw.Reddit(user_agent=user_agent)
 MIN_SCORE = 1 # min score for pup pictures
 subreddit = "rarepuppers"
-
+old_doggo = "string"
 
 def yielding(ls):
     for i in ls:
         yield i
 
-def scrape_pic():
+def scrape_pic(old_doggo):
 
     submissions = reddit.get_subreddit(subreddit).get_new(limit=300)
     #now let's parse through what we scraped and decide what we return
 
     temp_list = list(yielding(submissions))
-    starting_index = random.randint(0,280)
 
-    for submission in temp_list[starting_index:]:
+    random.shuffle(temp_list)
+    for submission in temp_list:
         if "gifv" in submission.url:
             continue
         if submission.score < MIN_SCORE:
@@ -38,29 +38,20 @@ def scrape_pic():
             url = imgurUrlPattern.search(submission.url).group(1)
             # 
             dac = DoggoAPICall(url)
+            if(dac.url == old_doggo):
 
+                continue
             if dac.verify_dog():
+
+                old_doggo = dac.url
                 return dac
             else:
                 print("not a doggo", url)
-            #checks for the case with a '?' in the filename
+
             
     return None
 
 
-'''
-setting up machine learning stuff on pictures
-'''
-
-
-def create_message():
-    """
-    create pupper message
-    :return:
-    """
-
-
-    return None
 
 
 '''
@@ -74,7 +65,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    doggo_object = scrape_pic()
+    doggo_object = scrape_pic(old_doggo)
     return render_template('pupperPage.html', image_url=doggo_object.url, doggo_message=doggo_object.bork_message())
 
 if __name__ == '__main__':
